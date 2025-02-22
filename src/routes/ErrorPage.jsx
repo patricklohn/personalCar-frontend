68087f0d994f2532777a9bb7aafb34fbb0a64ef4
patricklohn <patricklohn3@gmail.com>
2025-02-22 17:30:26 -0300

    ajuste de desing nas telas da home e error page

--- src/routes/ErrorPage.jsx
+++ src/routes/ErrorPage.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import './ErrorPage.css'
 
 const ErrorPage = () => {
+
   return (
     <div>
-      <h1>Error</h1>
+      <div className="error-container">
+        <div className='error'>
+          <h1>Ocorreu um erro não esperado ⚠️</h1>
+          <p>Favor voltar para a tela inicial.</p>
+          <Link to="/" className='btn-secondary'>Tela inicial</Link>
+        </div>
+      </div>
     </div>
   )
 }
